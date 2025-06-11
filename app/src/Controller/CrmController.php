ace115c10d6d9fdcaf62c5496303e972ff5b450c
Viacheslav Rukavichkin <s@u-wr.ru>
2025-06-11 19:21:02 +0300

    Add connect for React

--- app/src/Controller/CrmController.php
+++ app/src/Controller/CrmController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CrmController extends AbstractController
 {
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    #[Route('/crm', name: 'app_crm')]
+    #[Route('/crm/{reactRoute}', name: 'app_crm', requirements: ['reactRoute' => '.+'], defaults: ['reactRoute' => null])]
     public function index(): Response
     {
+        // Рендерим шаблон-оболочку для нашего SPA
         return $this->render('crm/index.html.twig');
     }
 }
