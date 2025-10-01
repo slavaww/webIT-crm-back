b0a4486b71140360da5b4bb0ebbcabef34c8aa2d
Viacheslav Rukavichkin <s@u-wr.ru>
2025-10-01 12:18:05 +0300

    Add task filter for role-user

--- app/src/State/TasksCollectionProvider.php
+++ app/src/State/TasksCollectionProvider.php
@@ -23,15 +23,59 @@ final class TasksCollectionProvider implements ProviderInterface
         $user = $this->security->getUser();
         $roles = $user->getRoles();
 
+        // Проверяем фильтры из query параметров
+        $filters = $context['filters'] ?? [];
+        // Проверяем статусы из query параметров
+        $statuses = $filters['statuses'] ?? [];
+        if (!empty($statuses)) {
+            $statuses = explode(',', $statuses);
+        }
+        $create_start = $filters['create_start'] ?? null;
+        $create_end = $filters['create_end'] ?? null;
+        $worker = $filters['worker'] ?? null;
+        $client = $filters['client'] ?? null;
+
         if (in_array('ROLE_SUPER_ADMIN', $roles)) {
             // Супер-админ видит все
             return $this->repository->findAll();
         } elseif (in_array('ROLE_ADMIN', $roles)) {
             // Сотрудник видит только свои задачи (где worker === user)
-            return $this->repository->findBy(['worker' => $user->getEmployee()]); // Предполагаем, что у User есть getEmployee()
+            return $this->repository->findBy(['worker' => $user->getEmployee()]);
         } elseif (in_array('ROLE_USER', $roles)) {
             // Клиент видит только свои задачи (где client === user)
-            return $this->repository->findBy(['client' => $user->getClient()]); // Предполагаем, что у User есть getClient()
+            // return $this->repository->findBy(['client' => $user->getClient()]);
+            $qb = $this->repository->createQueryBuilder('t')
+                    ->where('t.client = :client')
+                    ->setParameter('client', $user->getClient());
+
+            if (!empty($statuses)) {
+                $qb->andWhere('t.status IN (:statuses)')
+                ->setParameter('statuses', $statuses);
+            }
+
+            if (!empty($create_start) && !empty($create_end)) {
+                $qb->andWhere('t.create_date BETWEEN :create_start AND :create_end');
+                $qb->setParameter('create_start', $create_start);
+                $qb->setParameter('create_end', $create_end);
+            } elseif (!empty($create_start)) {
+                $qb->andWhere('t.create_date >= :create_start');
+                $qb->setParameter('create_start', $create_start);
+            } elseif (!empty($create_end)) {
+                $qb->andWhere('t.create_date <= :create_end');
+                $qb->setParameter('create_end', $create_end);
+            }
+
+            if (!empty($worker)) {
+                if ($worker == 1) {
+                    # worker is set
+                    $qb->andWhere('t.worker IS NOT NULL');
+                } else {
+                    # worker is not set
+                    $qb->andWhere('t.worker IS NULL');
+                }
+            }
+
+            return $qb->getQuery()->getResult();
         }
 
         return []; // Нет доступа — пустой список
